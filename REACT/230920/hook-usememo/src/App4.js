2c3bdfc1ef8d51f34d16fcda688ed59f671d4200
HyoEun Cho <chohe3212@naver.com>
2023-09-20 18:39:16 +0900

    usememo

---
+++ REACT/230920/hook-usememo/src/App4.js
@@ -0,0 +1,34 @@
+import { useContext, createContext } from "react";
+import { DarkMode } from "./DarkMode";
+
+const UserInfo = createContext({ name: "gary", id: "garyIsFree" });
+
+const App4 = () => {
+  return <HelloLicat />;
+};
+
+// const HelloLicat = () => {
+//   return (
+//     <UserInfo.Consumer>
+//       {(value) => (
+//         <div>
+//           <input type="text" />
+//           <h2>{value.name}</h2>
+//           <strong>{`@ ${value.id}`}</strong>
+//         </div>
+//       )}
+//     </UserInfo.Consumer>
+//   );
+// };
+
+const HelloLicat = () => {
+  const { name, id } = useContext(UserInfo);
+  return (
+    <div>
+      <h2>{name}</h2>
+      <strong>{id}</strong>
+    </div>
+  );
+};
+
+export default App4;
